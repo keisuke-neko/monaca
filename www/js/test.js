913d9b5896abc63be161c3475ef03fe83dfd18d8
keisuke-neko <state.of.0@gmail.com>
2017-05-09 12:48:31 +0900

    Project's Initial Commit

---
+++ www/js/test.js
@@ -0,0 +1,18 @@
+/*
+(function($) {
+    $('#test_input').val('foo');
+    $('#test_button').bind('touchstart', function() {
+        $('#test_input').val('hoge');
+    });
+})(jQuery);
+*/
+/*
+$(document).on('mobileinit', function() {
+  $.mobile.loader.prototype.options.text = 'loading';
+  $.mobile.loader.prototype.options.textonly = false;
+  $.mobile.loader.prototype.options.textVisible = true;
+  $.mobile.loader.prototype.options.theme = 'a';
+});
+*/
+
+$('#test_a').text('test');
